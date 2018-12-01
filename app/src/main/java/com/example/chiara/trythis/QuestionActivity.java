4e428081f30798af86386dd329c8abdc80fd450e
sjflaherty <sjflaherty@loyola.edu>
2018-12-01 13:03:21 -0500

    progress

--- app/src/main/java/com/example/chiara/trythis/QuestionActivity.java
+++ app/src/main/java/com/example/chiara/trythis/QuestionActivity.java
@@ -1,6 +1,8 @@
 package com.example.chiara.trythis;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -21,4 +23,39 @@ public class QuestionActivity extends Activity {
 
         questionTV.setText("Here is your question" /*+ smartySmart.getQuestion()*/ );
     }
+
+
+    public void checkAnswer( View v)
+    {
+        EditText wordET = (EditText) findViewById(R.id.answer_enter);
+        String answer = wordET.getText().toString();
+        if (answer.equals("hello"))
+        {
+            AlertDialog alertDialog = new AlertDialog.Builder(QuestionActivity.this).create();
+            alertDialog.setTitle("Check");
+            alertDialog.setMessage("Correct!");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+        else
+        {
+            AlertDialog alertDialog = new AlertDialog.Builder(QuestionActivity.this).create();
+            alertDialog.setTitle("Check");
+            alertDialog.setMessage("Sorry that's wrong!");
+            alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            alertDialog.show();
+        }
+
+    }
+
 }
