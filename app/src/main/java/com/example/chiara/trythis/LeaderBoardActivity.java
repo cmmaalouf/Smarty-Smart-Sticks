5808c9b4d183112e21537b73ef67ddaea845d2c2
Chiara <cmmaalouf@loyola.edu>
2018-12-06 05:30:19 -0500

    persistent data almost finished

--- app/src/main/java/com/example/chiara/trythis/LeaderBoardActivity.java
+++ app/src/main/java/com/example/chiara/trythis/LeaderBoardActivity.java
@@ -7,6 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class LeaderBoardActivity extends AppCompatActivity {
     private SharedPreferences sharedPref;
     @Override
@@ -14,11 +19,47 @@ public class LeaderBoardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        Map<String, ?> keys = sharedPref.getAll();
+        updateLeaderBoard();
+    }
+    public void updateLeaderBoard()
+    {
+        Map<String, ?> allScores = sharedPref.getAll();
+        ArrayList<String> top5names = new ArrayList<>();
+        top5names.add("       ");
+        top5names.add("       ");
+        top5names.add("       ");
+        top5names.add("       ");
+        top5names.add("       ");
+        for(int i =0; i<5; i++)
+        {
+            if(allScores.size()>0) {
+                String top = findMax(allScores);
+                top5names.set(i, top);
+                allScores.remove(top);
+            }
+        }
         TextView high_score = (TextView)(findViewById(R.id.first));
-        String bestScore = "Best Score: "+sharedPref.getInt("chiara",0);
+        String bestScore = top5names.get(0)+"\t"+sharedPref.getInt(top5names.get(0),0);
         high_score.setText(bestScore);
     }
 
+    public String findMax(Map<String,? > map)
+    {
+        int max =0;
+        String maxName = "";
+        for( String key : map.keySet()) {
+            if(map.get(key) instanceof Integer) {
+                if ((Integer)(map.get(key)) > max) {
+                    max = (Integer)(map.get(key));
+                    maxName = key;
+                }
+            }
+        }
+
+        return maxName;
+    }
     public void goHome(View view)
     {
         this.finish();
